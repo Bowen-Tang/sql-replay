f14ee48968f18e8fccc1d17f8608ac44b4453a27
Bowen-Tang <52245161+Bowen-Tang@users.noreply.github.com>
2024-01-29 21:18:55 +0800

    增加 replay speed 控制

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 
     // 共用的标志
     var slowLogPath, slowOutputPath, dbConnStr, replayOutputFilePath, filterUsername, filterSQLType,filterDBName, outDir, replayOut, tableName string
+    var Speed float64
     flag.StringVar(&slowLogPath, "slow-in", "", "Path to slow query log file")
     flag.StringVar(&slowOutputPath, "slow-out", "", "Path to slow output JSON file")
     flag.StringVar(&dbConnStr, "db", "username:password@tcp(localhost:3306)/test", "Database connection string")
@@ -19,9 +20,10 @@ func main() {
     flag.StringVar(&replayOut, "replay-name", "", "replayout filename of the JSON files")
     flag.StringVar(&tableName, "table", "", "Name of the table to insert data into")
     flag.StringVar(&replayOutputFilePath, "replay-out", "", "Path to output json file")
-    flag.StringVar(&filterUsername, "username", "all|username", "Username to filter (default 'all')")
-    flag.StringVar(&filterSQLType, "sqltype", "all|select", "SQL type to filter (default 'all')")
-    flag.StringVar(&filterDBName, "dbname", "all|dbname", "SQL type to filter (default 'all')")
+    flag.StringVar(&filterUsername, "username", "all", "Username to filter (default 'all',or username)")
+    flag.StringVar(&filterSQLType, "sqltype", "all", "SQL type to filter (default 'all',or select)")
+    flag.StringVar(&filterDBName, "dbname", "all", "SQL type to filter (default 'all',or dbname)")
+    flag.Float64Var(&Speed, "speed", 1.0, "Replay speed, default 1.0")
     flag.Parse()
 
     if mode == "" {
@@ -33,7 +35,7 @@ func main() {
     case "parse":
         ParseLogs(slowLogPath, slowOutputPath)
     case "replay":
-        ReplaySQL(dbConnStr, slowOutputPath, replayOutputFilePath, filterUsername, filterSQLType, filterDBName)
+        ReplaySQL(dbConnStr, Speed, slowOutputPath, replayOutputFilePath, filterUsername, filterSQLType, filterDBName)
     case "load":
         LoadData(dbConnStr, outDir, replayOut, tableName)
     default:
