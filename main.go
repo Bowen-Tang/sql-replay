d589ceedc32fa390d950654d7dbd6f6bb39814ef
goroutine <ngaut@126.com>
2024-08-27 19:36:21 -0700

    Make language configurable in ReplaySQL function
    - Add -lang flag to specify output language
    - Update ReplaySQL call to use lang variable
    - Set default language to English ('en')
    - Update usage information for replay mode

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
     // 共用的标志
     var slowLogPath, slowOutputPath, dbConnStr, replayOutputFilePath, filterUsername, filterSQLType,filterDBName, outDir, replayOut, tableName, Port string
     var Speed float64
+    var lang string  // New variable for language
 
     // 增加版本
     var showVersion bool
@@ -33,6 +34,7 @@ func main() {
     flag.StringVar(&filterDBName, "dbname", "all", "SQL type to filter (default 'all',or dbname)")
     flag.Float64Var(&Speed, "speed", 1.0, "Replay speed, default 1.0")
     flag.StringVar(&Port, "port", ":8081", "Report Web port")
+    flag.StringVar(&lang, "lang", "en", "Language for output (e.g., 'en' for English, 'zh' for Chinese)")  // New flag
 
     flag.Parse()
 
@@ -44,7 +46,7 @@ func main() {
     if mode == "" {
         fmt.Println("Usage: ./sql-replay -mode [parse|replay|load|report]")
         fmt.Println("    1. parse mode: ./sql-replay -mode parse -slow-in <path_to_slow_query_log> -slow-out <path_to_slow_output_file>")
-        fmt.Println("    2. replay mode: ./sql-replay -mode replay -db <mysql_connection_string> -speed 1.0 -slow-out <slow_output_file> -replay-out <replay_output_file> -username <all|username> -sqltype <all|select> -dbname <all|dbname>")
+        fmt.Println("    2. replay mode: ./sql-replay -mode replay -db <mysql_connection_string> -speed 1.0 -slow-out <slow_output_file> -replay-out <replay_output_file> -username <all|username> -sqltype <all|select> -dbname <all|dbname> -lang <en|zh>")
         fmt.Println("    3. load mode: ./sql-replay -mode load -db <DB_CONN_STRING> -out-dir <DIRECTORY> -replay-name <REPORT_OUT_FILE_NAME> -table <replay_info>")
         fmt.Println("    4. report mode: ./sql-replay -mode report -db <mysql_connection_string> -replay-name <replay name> -port ':8081'")
         os.Exit(1)
@@ -54,7 +56,7 @@ func main() {
     case "parse":
         ParseLogs(slowLogPath, slowOutputPath)
     case "replay":
-        ReplaySQL(dbConnStr, Speed, slowOutputPath, replayOutputFilePath, filterUsername, filterSQLType, filterDBName, "en")
+        ReplaySQL(dbConnStr, Speed, slowOutputPath, replayOutputFilePath, filterUsername, filterSQLType, filterDBName, lang)
     case "load":
         LoadData(dbConnStr, outDir, replayOut, tableName)
     case "report":
